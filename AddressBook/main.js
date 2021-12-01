6eefdd379787b37041090d562dfc8fb3638aaa83
UllasUKrishnan <krishnanullas3@gmail.com>
2021-12-01 10:29:21 +0530

    Address project bug Fixed

--- AddressBook/main.js
+++ AddressBook/main.js
@@ -1,9 +1,20 @@
 document.getElementById('filterInput').addEventListener('keyup', () => {
-    let filterValue = document.getElementById('filterInput').value.toUpperCase();
-    let ul = document.getElementById('names');
-    let li = ul.querySelectorAll('li.collection-item');
-    li.forEach(item => {
-        let a = item.getElementsByTagName('a')[0];
-        item.style.display = a.innerHTML.toUpperCase().startsWith(filterValue) ? 'block' : 'none';
-    })
-})
+  let filterValue = document.getElementById('filterInput').value.toUpperCase();
+  let ul = document.getElementById('names');
+  let li = ul.querySelectorAll('li.collection-item');
+  let li_Header = ul.querySelectorAll('li.collection-header');
+  li.forEach((item) => {
+    let a = item.getElementsByTagName('a')[0];
+    let compare = a.innerHTML.toUpperCase().startsWith(filterValue);
+    item.style.display = compare ? 'block' : 'none';
+  });
+
+  li_Header.forEach((item) => {
+    let h5 = item.getElementsByTagName('h5')[0];
+    item.style.display = h5.innerHTML
+      .toUpperCase()
+      .startsWith(filterValue.slice(0, 1))
+      ? 'block'
+      : 'none';
+  });
+});
--- Converter/main.js
+++ Converter/main.js
@@ -1,9 +1,14 @@
-document.getElementById('kgsInput').addEventListener('input', e => {
-    let kgs = e.target.value;
-    document.getElementById('lbsOutput').innerHTML = kgs * 2.2046;
-})
+document.getElementById('kgsInput').addEventListener('input', (e) => {
+  let kgs = e.target.value;
+  document.getElementById('lbsOutput').innerHTML = kgs * 2.2046;
+});
 
-document.getElementById('feetInput').addEventListener('input', e => {
-    let feet = e.target.value;
-    document.getElementById('cmsOutput').innerHTML = feet * 30.48;
-})
+document.getElementById('feetInput').addEventListener('input', (e) => {
+  let feet = e.target.value;
+  document.getElementById('cmsOutput').innerHTML = feet * 30.48;
+});
+
+document.getElementById('tempInput').addEventListener('input', (e) => {
+  let temp = e.target.value;
+  document.getElementById('tempOutput').innerHTML = (temp * 9) / 5 + 32;
+});
