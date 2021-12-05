e20ffcfd928591d9f161029264e4f3a6d9db0c15
UllasUKrishnan <krishnanullas3@gmail.com>
2021-12-05 13:50:17 +0530

    Html and css created form MovieApp

---
+++ Movie App/index.js
@@ -0,0 +1,7 @@
+// const API_KEY = '04c35731a5ee918f014970082a0088b1';
+// const BASE_URL = 'https://api.themoviedb.org/3';
+// const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+
+// fucntion getMoVies(url){
+//  fetch(url).then((res)=>res.json())
+// }
